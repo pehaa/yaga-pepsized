1b824a04f62f2e7d1ba7c5178007395bcd47582b
PeHaa <info@pehaa.com>
2015-06-17 14:14:16 +0200

    replaced by the right version od child

--- functions.php
+++ functions.php
@@ -1,3 +1,9 @@
 <?php
-add_filter( 'faktory_load_stylesheet_css', '__return_false' );
-?>
+
+/* To start customizing your css, please edit css/style.css. Nothing needs to be done here. */
+
+/* Uncomment the following line if you want to write your css from scratch, the parent stylesheet will not be loaded. */
+/*  add_filter( 'pht_load_template_css', '__return_false' ); */
+
+/* Uncomment the following line if you do not customize your theme stylesheet at all, the child-theme stylesheet will not be loaded. */
+/*  add_filter( 'pht_load_stylesheet_css', '__return_false' ); */
